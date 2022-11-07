0950dd347786740007ba1c176c563c597ff8bec3
hgokumus35 <gokalp.okumus@topkapidanismanlik.com.tr>
2022-11-07 09:19:07 +0300

    project base and some designs

---
+++ app/src/main/java/com/loodos/cryptocurrency/ui/market/MarketFragment.kt
@@ -0,0 +1,8 @@
+package com.loodos.cryptocurrency.ui.market
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MarketFragment: Fragment() {
+}
